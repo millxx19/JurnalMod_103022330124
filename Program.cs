bb4f88eca4366a0cff6c914e7f3bec1929fe6c3e
RafaMufid <akurafamufid@gmail.com>
2025-03-20 10:42:08 +0700

    commit ulangg

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class PemrosesData
+{
+    public dynamic DapatNilaiTerbesar<T>(T x1, T x2, T x3)
+    {
+        dynamic bilangan1 = x1;
+        dynamic bilangan2 = x2;
+        dynamic bilangan3 = x3;
+
+        if (bilangan1 > bilangan2 && bilangan1 > bilangan3)
+        {
+            return bilangan1;
+        }
+        else if (bilangan2 > bilangan3 && bilangan2 > bilangan1)
+        {
+            return bilangan2;
+        }
+        else
+        {
+            return bilangan3;
+        }
+    }
+
+    public class program
+    {
+        public static void Main()
+        {
+            PemrosesData data = new PemrosesData();
+            Console.WriteLine("Nilai terbesar adalah: " + data.DapatNilaiTerbesar<double>(10, 30, 22));
+        }
+    }
+}
